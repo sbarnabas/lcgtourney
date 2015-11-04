7191b02243e7fb333089701ae5c68a39a84bfcde
Sandy Barnabas <sbarnabas@gmail.com>
2015-11-04 06:32:52 -0500

    added bower components
    Added polymer, bower, etc.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask,request,render_template	
+from flask import Flask, render_template
+from flask.ext.bower import Bower
 app = Flask(__name__, static_path='/static')
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -11,6 +12,16 @@ app = Flask(__name__, static_path='/static')
 def hello():
     """Return a friendly HTTP greeting."""
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return 'Nothing here'
+
+
+@app.route('/email')
+def email():
+    return 'nothing here'
 
 
 @app.errorhandler(404)
@@ -23,3 +34,6 @@ def page_not_found(e):
 def application_error(e):
     """Return a custom 500 error."""
     return 'Sorry, unexpected error: {}'.format(e), 500
+
+
+Bower(app)
--- static/main.js
+++ static/main.js
@@ -1,18 +1,15 @@
 (function () {
-
-  'use strict';
-
-  angular.module('lcgtourney', [])
-  .config(function($interpolateProvider) {
-    $interpolateProvider.startSymbol('[[');
-    $interpolateProvider.endSymbol(']]');
-  })
-  .controller('TourneyController', ['$scope', '$log', function($scope, $log) {
-    $scope.getResults = function() {
-      $log.log("test");
-    };
-  }
-
-  ]);
-
+    'use strict';
+    angular.module('lcgtourney', [])
+        .config(function ($interpolateProvider) {
+            $interpolateProvider.startSymbol('[[');
+            $interpolateProvider.endSymbol(']]');
+        })
+        .controller('TourneyController', ['$scope', '$log',
+            function ($scope, $log) {
+                $scope.getResults = function () {
+                    $log.log("test");
+                };
+            }
+        ]);
 }());
